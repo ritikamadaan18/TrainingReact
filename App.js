5ab8db5400526379f7b30bc59d5d64d3a82b97ea
ritikamadaan18 <rashishvi@gmail.com>
2025-01-31 20:12:11 -0500

    Adding React and package bundler

--- App.js
+++ App.js
@@ -1,4 +1,11 @@
-const heading = React.createElement("h1", {}, "hello worrrld from react");
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement(
+  "h1",
+  { style: { color: "blue" } },
+  "hello world from react"
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
