27a60d37f38587a7295a81b7c92356d1c1317867
ritikamadaan18 <rashishvi@gmail.com>
2025-02-15 13:23:56 -0500

    Added Tailwind

--- src/components/Body.js
+++ src/components/Body.js
@@ -47,19 +47,19 @@ const Body = () => {
     <Shimmer />
   ) : (
     <div className="body">
-      <div className="filter">
-        <div className="search">
+      <div className="flex">
+        <div className="m-4 p-4">
           <input
             type="text"
             data-testid="searchInput"
-            className="searchInput"
+            className="border border-solid border-black"
             value={searchText}
             onChange={(e) => {
               setSearchText(e.target.value);
             }}
           />
           <button
-            className="m"
+            className="px-4 py-2 bg-green-100 m-4 rounded-lg"
             onClick={() => {
               // Filter the restraunt cards and update the UI
               const filteredRestaurant = listOfRestaurants.filter((res) =>
@@ -72,19 +72,20 @@ const Body = () => {
             Search
           </button>
         </div>
-
-        <button
-          className="filter-btn"
-          onClick={() => {
-            setFilteredRestaurant(
-              listOfRestaurants.filter((res) => res?.info?.avgRating > 4)
-            );
-          }}
-        >
-          Top Rated Restaurants
-        </button>
+        <div className="m-4 p-4 flex items-center">
+          <button
+            className="px-4 py-2 bg-gray-100 rounded-lg"
+            onClick={() => {
+              setFilteredRestaurant(
+                listOfRestaurants.filter((res) => res?.info?.avgRating > 4)
+              );
+            }}
+          >
+            Top Rated Restaurants
+          </button>
+        </div>
       </div>
-      <div className="res-container">
+      <div className="flex flex-wrap">
         {filteredRestaurant?.map((restaurant) => (
           <Link
             key={restaurant?.info.id}
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -5,14 +5,14 @@ const RestaurantCard = (props) => {
   const { avgRating, costForTwo, cloudinaryImageId, sla, cuisines, name } =
     resData?.info;
   return (
-    <div className="res-card">
+    <div className="m-4 p-4 w-[250px] rounded-lg bg-gray-100 hover:bg-gray-200">
       <img
-        className="res-logo"
+        className="rounded-lg"
         alt="res-logo"
         src={CDN_URL + cloudinaryImageId}
         // src="https://www.dosafactorymenu.com/blog-admin/images/indian-food-items-apt-for-everyone041641.jpeg"
       />
-      <h3>{name}</h3>
+      <h3 className="font-bold py-4 text-lg">{name}</h3>
       <h5>{cuisines.join(",")}</h5>
       <h5>{avgRating} stars</h5>
       <h5>{costForTwo}</h5>
